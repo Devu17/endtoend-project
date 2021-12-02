c810ca7392d63ffca18e8c1293b5b4e57c44933e
Devu17 <devupp@gmail.com>
2021-12-02 19:28:04 +0530

    full work

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     framework: 'jasmine', //Type of Framework used 
     directConnect:true, 
-    specs: ['spec.js'], //Name of the Specfile 
+    specs: ['login.js','viewallorder.js','edit.js','viewallproducts.js','order.js'], //Name of the Specfile 
     onPrepare() { 
           require('ts-node').register({ 
           project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file 
